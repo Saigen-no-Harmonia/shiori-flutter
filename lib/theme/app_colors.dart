af415934f5c1a9b99f9d3c0ff0d3e7646739b408
canacel3 <nishidakaede3@gmail.com>
2025-09-21 09:59:44 +0900

    Color定義追加

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primary = Color(0xFFC87F83);
+  static const secondary = Color(0xFFA8C97F);
+  static const tertiary = Color(0xFFCCC981);
+  static const textPrimary = Color(0xFF212121);
+  static const textPrimary80 = Color(0xCC212121);
+  static const background = Color(0xFFFDFEF0);
+  static const white = Color(0xFFFFFFFF);
+}
