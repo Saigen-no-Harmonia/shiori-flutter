8c37c5ccf58ec942feff86275e01e043fc01ac74
canacel3 <nishidakaede3@gmail.com>
2025-09-21 11:25:55 +0900

    フォント設定追加

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static final bodyBold = GoogleFonts.notoSerifJp(
+    fontSize: 17,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final body = GoogleFonts.notoSerifJp(
+    fontSize: 17,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final calloutBold = GoogleFonts.notoSerifJp(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final caption = GoogleFonts.notoSerifJp(
+    fontSize: 12,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final headline = GoogleFonts.notoSerifJp(
+    fontSize: 17,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final largeTitleBold = GoogleFonts.notoSerifJp(
+    fontSize: 34,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final title2Bold = GoogleFonts.notoSerifJp(
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final title3Bold = GoogleFonts.notoSerifJp(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  );
+
+  static final title3 = GoogleFonts.notoSerifJp(
+    fontSize: 20,
+    fontWeight: FontWeight.normal,
+  );
+
+  static final titleBold = GoogleFonts.notoSerifJpe(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+  );
+}
